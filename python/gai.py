312af25c6b47145c73cb048fab3ffdd2b89f0b95
Victor Lambret <lambret.victor@gmail.com>
2016-03-13 14:50:32 +0100

    First basis of interpreter

--- python/gai.py
+++ python/gai.py
@@ -1,7 +1,23 @@
 from commit import *
+import os, sys
+
+class gai:
+
+    def __init__(self, file):
+        self.file = file
+
+    def run(self):
+        c1 = commit("c1", 50, 50, "black", None)
+        c1.toSVG()
 
 def main():
-    c1 = commit("c1", 50, 50, "black", None)
-    c1.toSVG()
+    if len(sys.argv) != 2:
+        print "Please specify a gai script file"
+        exit()
+    else:
+        gai_file = sys.argv[1]
+
+    interpreter = gai(gai_file)
+    interpreter.run()
 
 main()

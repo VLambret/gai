62ba1d69036907000f9fbc0cd27727cb54045b65
Victor Lambret <lambret.victor@gmail.com>
2016-03-14 15:14:54 +0100

    Interpreter inherits Cmd class

--- python/commit.py
+++ python/commit.py
@@ -2,12 +2,12 @@
 
 class commit:
 
-    def __init__(self, id, x, y, color, parent ):
-        self.id = id
+    def __init__(self, tag, x, y, color, parent ):
+        self.tag = tag
         self.x = x
         self.y = y
         self.color = color
         self.parent = parent
 
     def toSVG(self):
-        print "<circle cx=\"{}\" cy=\"{}\" r=\"20\" stroke=\"black\" stroke-width=\"3\" fill=\"{}\" />".format(self.x, self.y, self.color)
+        print("<circle cx=\"{}\" cy=\"{}\" r=\"20\" stroke=\"black\" stroke-width=\"3\" fill=\"{}\" />".format(self.x, self.y, self.color))
--- python/gai.py
+++ python/gai.py
@@ -1,23 +1,42 @@
 from commit import *
-import os, sys
+import cmd, sys
 
-class gai:
+class gai(cmd.Cmd):
 
-    def __init__(self, file):
-        self.file = file
+    def __init__(self):
+        self.commits = {}
+        super(gai, self).__init__()
+
+    def emptyline(self):
+        pass
+
+    def do_exit(self, s):
+        return True
+
+    do_EOF = do_exit
+
+    def do_commit(self, arg):
+        p = arg.split(' ')
+        tag = p[0]
+        x = p[1]
+        y = p[2]
+        color = p[3]
+        parent= p[4]
+        self.commits[tag] = commit(tag, x, y, color, parent)
+
+    def do_snapshot(self, arg):
+        for tag in self.commits:
+            print(tag)
+            self.commits[tag].toSVG()
 
     def run(self):
+        for line in self.f:
+            for w in line.split(' '):
+                print(w)
         c1 = commit("c1", 50, 50, "black", None)
         c1.toSVG()
 
 def main():
-    if len(sys.argv) != 2:
-        print "Please specify a gai script file"
-        exit()
-    else:
-        gai_file = sys.argv[1]
-
-    interpreter = gai(gai_file)
-    interpreter.run()
+    gai().cmdloop()
 
 main()
